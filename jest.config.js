3d06d43bad1710c2e22d39333bbdc3711b7f8318
Kevin Chen <kevin@fangzhou.io>
2021-02-24 01:06:10 -0500

    Add basic snapshot testing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,19 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
-  roots: ['<rootDir>/src'],
   testMatch: [
     '**/__tests__/**/*.+(ts|tsx|js)',
     '**/?(*.)+(spec|test).+(ts|tsx|js)',
   ],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css)$': 'identity-obj-proxy',
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^store/(.*)$': '<rootDir>/src/store/$1',
+    '^api/(.*)$': '<rootDir>/src/api/$1',
   },
 };
